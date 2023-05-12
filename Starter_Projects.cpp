6f6c41d17d983708571fc9e17daade477ee4436f
AwsomeTheKingVic <126616653+AwsomeTheKingVic@users.noreply.github.com>
2023-05-12 01:47:03 -0600

    Calculater Class

--- Starter_Projects.cpp
+++ Starter_Projects.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include "Calculater.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    //classes being added
+    Calculater Calculater;
+
+    //main class variables
+    bool exit = false;
+    int choice = 0;
+
+    //Menu
+    while (!exit) {
+
+        //askes for user choice to pick between clasess
+        std::cout << "What would you like to do\n";
+        std::cout << "Use Calculater(1)\n";
+
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        
+            //calculater
+            case 1:
+
+                Calculater.StartUp();
+
+                break;
+
+            default:
+
+                std::cout << "I guess it was nothing\n";
+
+                break;
+
+        }
+
+        //allows the user to leave or keep goin
+        std::cout << "Would you like to go exit(0 = false | 1 = true)\n";
+
+        std::cin >> exit;
+
+    }
+    
+
 }
